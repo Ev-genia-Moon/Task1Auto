d6193df73769dda6a33fe83bb1b32f0df1055df6
Eva.Kusyakina <eva.kusyakina@gmail.com>
2024-11-19 17:14:54 +0300

    Third commit TestNG

--- src/test/java/ru/netology/Task1Auto/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/Task1Auto/CashbackHackServiceTest.java
@@ -17,5 +17,17 @@ public class CashbackHackServiceTest{
         assertEquals(actual,expected);
 
     }
+
+    @Test
+    void calculateCashback() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+
+    }
 }
 
