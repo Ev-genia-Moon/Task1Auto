93ac29c24b16f22c9b156051a64214a3a8aa6cb8
Eva.Kusyakina <eva.kusyakina@gmail.com>
2024-11-19 17:49:52 +0300

    Change commit Unit4

--- src/test/java/ru/netology/Task1Auto/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/Task1Auto/CashbackHackServiceTest.java
@@ -7,26 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CashbackHackServiceTest{
 
     @Test
-    void calculateCashback() {
+    void calculateCashbackNGPositive() {
         CashbackHackService cashback = new CashbackHackService();
         int amount = 700;
 
         int actual = cashback.remain(amount);
         int expected = 300;
 
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
 
     }
 
     @Test
-    void calculateCashback() {
+    void calculateCashbackNGNegative() {
         CashbackHackService cashback = new CashbackHackService();
         int amount = 1000;
 
         int actual = cashback.remain(amount);
         int expected = 0;
 
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
 
     }
 }
